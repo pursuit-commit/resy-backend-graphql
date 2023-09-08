6875f4264c9493e8a0c0e455eb74af636241aea6
Billy Taggart <49992113+williamtaggart97@users.noreply.github.com>
2023-09-08 12:21:56 -0400

    test commit

--- src/restaurants/restaurant.module.ts
+++ src/restaurants/restaurant.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { RestaurantService } from './restaurant.service';
 import { RestaurantsResolver } from './restaurant.resolver';
 import { ReservationService } from 'src/reservations/reservation.service';
-
+// test
 @Module({
   providers: [
     RestaurantService,
